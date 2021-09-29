9e6669295c633f9c37dadb10903bd63886cddc05
Vitalii Tarasiuk <91137464+VitaliiPro@users.noreply.github.com>
2021-09-29 19:26:55 +0300

    HW 2.4

--- homework-2/test2.js
+++ homework-2/test2.js
@@ -1,29 +1,31 @@
 let firstNum = '';
 do {
     firstNum = +prompt('Введіть перше ціле число:');
-} while (isNaN(firstNum) || firstNum % 1 !== 0 || firstNum == '');
+} while (isNaN(firstNum) || firstNum % 1 !== 0 || firstNum.length === 0);
 console.log(firstNum);
 
 let secondNum = '';
 do {
     secondNum = +prompt('Введіть друге ціле число:');
-} while (isNaN(secondNum) || secondNum % 1 !== 0  || secondNum == '');
+} while (isNaN(secondNum) || secondNum % 1 !== 0  || secondNum.length === 0);
 console.log(secondNum);
+
+if (firstNum > secondNum) {
+    [firstNum, secondNum] = [secondNum, firstNum];
+};
 
 const missNumber = confirm('Чи потрібно пропускати парні числа?');
 
 let sum = 0;
-
+for(let i = firstNum; i <= secondNum; i++){
     if (missNumber === true) {
-        for (let i = firstNum; i <= secondNum; i++) {
-            if (i % 2 != 0) {
-                sum += +i;
-            };
+        if (i % 2 !== 0) {
+           sum += i;
         };
     } else {
-        for (let i = firstNum; i <= secondNum; i++) {
-            sum += +i;
-        };
+        sum += i;
     };
+};
 
+alert(`Sum is ${sum}`)
 console.log(sum);
