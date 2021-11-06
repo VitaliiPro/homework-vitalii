bd8d6d8b576de93ba5a7849aa5a6d072c8f3c1f5
Vitalii Tarasiuk <91137464+VitaliiPro@users.noreply.github.com>
2021-11-06 13:42:01 +0200

    HW#11.1

--- homework-11/test11.js
+++ homework-11/test11.js
@@ -1,8 +1,11 @@
 function getRandomChinese(length) {
-    let i = 0;
-    let str = '';
-    let promise = new Promise((resolve) => {
-        while (i < length) {
+    return new Promise((resolve, reject) => {
+        if (!Number.isInteger(length) || length <= 0) {
+            reject('You must enter an integer number');
+        }
+        let i = 1;
+        let str = '';
+        while (i <= length) {
             setTimeout(() => {
                 const dateTime = Date.now();
                 const sign = dateTime % 100000;
@@ -10,11 +13,11 @@ function getRandomChinese(length) {
                 if (str.length === length) {
                     resolve(str);
                 }
-            }, i * 50);
-        i++
-        }
+            }, 50 * i);
+        i++;
+        } 
     });
-    return promise;
 }
 
-getRandomChinese(4).then((result) => console.log(result));
+getRandomChinese(4).then((result) => console.log(result)).catch((error) => console.log(error));
+
